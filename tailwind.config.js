f053d35ae8e0816bd49a244c49b5effc2d74baa0
olaDmenace <olayinkafagbenro@gmail.com>
2024-03-12 14:08:56 +0100

    Website Done - Mobile only

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        // sans: ["Lora", "Open Sans"],
+        lora: ["Lora"],
+        openSans: ["Open Sans"],
+      },
+    },
+  },
+  plugins: [],
+};
